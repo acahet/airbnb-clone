fefb0b96771e41b08af2b3758bea3a45b8da3402
anderson-cahet <anderson.cahet@ghe.megaleo.com>
2020-11-04 11:30:30 +0000

    initial commit - showing how app will render

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
-
 import './App.css';
 
 function App() {
-  return (
-    <div className="app">
-      {/* header */}
-      {/* banner */}
-    </div>
-  );
+	return (
+		<div className="app">
+			{/* header */}
+			{/* banner */}
+			{/* card */}
+			{/* footer */}
+		</div>
+	);
 }
 
 export default App;
