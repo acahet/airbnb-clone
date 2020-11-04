f3dd58aa0fa9e8d1aa898ee3fea969c0738c5f9f
anderson-cahet <anderson.cahet@ghe.megaleo.com>
2020-11-04 17:36:13 +0000

    feat: home page
    - banner component add
    - show date btn

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Banner from '../Banner';
 import './styles.css';
 function Home() {
-	return <div className="home">
-        
-    </div>;
+	return (
+		<div className="home">
+			<Banner />
+		</div>
+	);
 }
 
 export default Home;
