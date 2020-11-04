a96078de8e8423525a14cf36dfa262d5b14acd0c
anderson-cahet <anderson.cahet@ghe.megaleo.com>
2020-11-04 20:10:00 +0000

    feat: footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
-
+import './styles.css';
 function Footer() {
-	return <div className="footer">Footer</div>;
+	return (
+		<div className="footer">
+			<p>© 2020 Airbnb clone! No rights reserved - this is a DEMO!</p>
+			<p>Privacy · Terms · Sitemap · Company Details</p>
+		</div>
+	);
 }
 
 export default Footer;
